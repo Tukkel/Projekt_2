b81c4e7611bc6745b35825f46a1545439a14a015
Bagg3 <andreasbaggenielsen@gmail.com>
2023-04-27 08:44:26 +0200

    Added comments to headerfile

--- IPRS/src/PC/User.h
+++ IPRS/src/PC/User.h
@@ -3,7 +3,8 @@
 class User
 {
 private:
-    std::string username_;
+    // Variables
+    std::string username_ = "admin"; // Username is always admin
     std::string password_;
     bool isLoggedIn_;
 
@@ -11,8 +12,16 @@ public:
     User();
     ~User();
 
+    // Function to login to the system, checks if the username and password are correct
+    // Return if the login is true og not
     bool login(std::string username, std::string password);
+
+    // Function to logout of the system
     void logout();
+
+    // Function to check if the user is logged in
     bool isLoggedIn();
+
+    // Function to change the password of the user, username
     void changePassword(std::string newPassword);
 };
