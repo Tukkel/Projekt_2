2edce206898b2b62054e0404b01093f80aa9b734
Andreas Bagge <andreasbaggenielsen@gmail.com>
2023-05-02 21:48:36 +0200

    Added comments to the dbHandler file

--- IPRS/src/PC/dbHandler.cpp
+++ IPRS/src/PC/dbHandler.cpp
@@ -3,6 +3,13 @@
 #include <iostream>
 #include <fstream>
 
+// Constructor with default dbHost - folder name of the database
+dbHandler::dbHandler(std::string dbHost)
+{
+    dbHost_ = dbHost;
+}
+
+// Function to cipher/decipher data with XOR cipher
 std::string dbHandler::xorCipher(std::string data, char key)
 {
     std::string result = data;
@@ -13,13 +20,10 @@ std::string dbHandler::xorCipher(std::string data, char key)
     return result;
 }
 
-dbHandler::dbHandler(std::string dbHost)
-{
-    dbHost_ = dbHost;
-}
-
+// Decrypt data from file, using key from key.txt, where fileAdress is the name of the file in UserDB folder
 std::string dbHandler::findData(std::string fileAdress)
 {
+    // Tries to open file, if it fails, throws an error
     std::string data;
     std::ifstream file;
     file.open(dbHost_ + fileAdress);
@@ -52,12 +56,14 @@ std::string dbHandler::findData(std::string fileAdress)
     {
         throw std::runtime_error("Unable to open file");
     }
-
+    // Returns the deciphered data
     return xorCipher(data, key);
 }
 
+// Encrypt data to file, using key from key.txt, where fileAdress is the name of the file in UserDB folder
 void dbHandler::saveData(std::string fileAdress, std::string data)
 {
+    // Load key from key file
     char key;
     std::ifstream keyFile;
     keyFile.open(dbHost_ + "key.txt");
@@ -72,7 +78,7 @@ void dbHandler::saveData(std::string fileAdress, std::string data)
     {
         throw std::runtime_error("Unable to open file");
     }
-
+    // Saves the ciphered data to file
     std::ofstream file;
     file.open(dbHost_ + fileAdress);
     if (file.is_open())
