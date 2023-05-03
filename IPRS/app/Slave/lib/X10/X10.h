b7021eac1666275a9bf85347c46657e3314f3095
Junale <mr.simon.moller@gmail.com>
2023-05-03 10:41:41 +0200

    Tilføjet slave

--- IPRS/app/Slave/lib/X10/X10.h
+++ IPRS/app/Slave/lib/X10/X10.h
@@ -2,21 +2,21 @@
 #include <string>
 #include <vector>
 
-class TX10
+class X10
 {
 private:
     std::string rx_pin_;
     std::string tx_pin_;
     std::string clock_;
-    bool *address_;
+    int *address_;
     char unit_;
 
     bool readBit();
     void writeBit(bool bit);
 
 public:
-    TX10(std::string rx_pin, std::string tx_pin, std::string clock, bool *address, char unit);
-    ~TX10();
-    std::vector<int> readData();
+    X10(std::string rx_pin, std::string tx_pin, std::string clock, int *address, char unit);
+    ~X10();
+    bool* readData();
     void writeData(bool *data, bool *address);
 };
