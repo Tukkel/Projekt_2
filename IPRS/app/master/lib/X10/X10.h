b36f0cb37bd9a6bf9a80bc82d6f34eaf41e98245
Tukkel <terkel1234@gmail.com>
2023-05-04 14:56:53 +0200

    X10 working

--- IPRS/app/master/lib/X10/X10.h
+++ IPRS/app/master/lib/X10/X10.h
@@ -9,15 +9,15 @@ class X10
 {
     public:
         X10(volatile uint8_t clock_pin, volatile uint8_t X10_read, volatile uint8_t X10_write, bool* address, char unit = 's');
-        bool readBit() const;
+        bool readHalfBit() const;
         bool* readData();
-        void writeBit(bool bit);
-        void writeData(bool* data, bool* address = 0);
-        
+        void writeBit(uint8_t bit) const;
+        void writeData(bool* data, bool* address = 0) const;
     private:
         volatile uint8_t clock_pin_;
         volatile uint8_t X10_read_;
         volatile uint8_t X10_write_;
-        bool* address_;
+        bool address_[8] = {false};
         char unit_;
+        bool data_[100] = {false};
 };
--- IPRS/app/master/src/main.cpp
+++ IPRS/app/master/src/main.cpp
@@ -5,70 +5,21 @@
 * Henning Hargaard 11/4 2019                *
 *********************************************/
 #include <avr/io.h>
-#define F_CPU 16000000
-#include <util/delay.h>
+#include "X10.h"
 #include "uart.h"
-
-void Init_ADC()
-{
-	// PF pins are inputs (ADC)
-	DDRF = 0;
-	// Internal 5 volt reference, ADLAR = 0, Input = ADC0 single ended
-	ADMUX = 0b01000000;
-	// ADC enable
-	// ADC interrupt disabled
-	// ADC prescaler = 128 (=> ADC clock = 16 MHz / 128 = 125 kHZ)
-	ADCSRA = 0b10000111;
-}
-
-void Init_Timer3()
-{
-	// OC3A = PE, pin 3 : Set as output pin
-	DDRE |= (1<<3);
-	// Mode = 3 (PWM, Phase Correct, 10 bit)
-	// Set OC3A on match down counting / Clear OC3A on match up counting
-	// Clock prescaler = 1
-	TCCR3A = 0b10000011;
-	TCCR3B = 0b00000001;
-	// Duty cycle = 50%
-	OCR3A = 1024/2;
-}
 
 int main()
 {
-	unsigned int ADC_Potentiometer, ADC_PWM;
-	int dif;
-	
-	Init_ADC();
-	Init_Timer3();
 	UART UART(9600, 8);
-	UART.SendChar('\r');
+	bool address[8] = {false};
+	X10 X10(1, 2, 4, address, 'm');
+
 	while(1)
 	{
-		ADMUX &= ~(1);
-		ADCSRA |= 0b01000000;
-		while (ADCSRA & 0b01000000)
-		{}
-		ADC_Potentiometer = ADCW;
-		UART.SendString("ADC0 = ");
-		UART.SendInteger(ADC_Potentiometer);
-		ADMUX |= 1;
-		ADCSRA |= 0b01000000;
-		while (ADCSRA & 0b01000000)
-		{}
-		ADC_PWM = ADCW;
-		UART.SendString(" | ADC1 = ");
-		UART.SendInteger(ADC_PWM);
+		for(size_t i=0; i<10; ++i)
+		{
+			UART.SendInteger(X10.readHalfBit());
+		}
 		UART.SendChar('\n');
-		dif = ADC_Potentiometer-ADC_PWM;
-		if (dif > 0)
-		{
-			OCR3A += (1+dif/3);
-		}
-		else if (dif < 0)
-		{
-			OCR3A += (dif/3-1);
-		}
-		_delay_ms(1000);
 	}
 }
