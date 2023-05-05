58ed57656c120ad3b23437d8dbf1202c3bf9b092
Andreas Bagge <andreasbaggenielsen@gmail.com>
2023-05-05 13:58:28 +0200

    Small fixes

--- IPRS/src/PC/PCHandler.cpp
+++ IPRS/src/PC/PCHandler.cpp
@@ -95,6 +95,62 @@ void PCHandler::printData() const
     }
 }
 
+void PCHandler::changeSystem() const
+{
+    std::cout << "Changeing System..." << std::endl;
+
+    int choice = 0;
+    bool goBack = false;
+
+    while (!goBack)
+    {
+        clearScreen();
+        std::cout << "Welcome to your program!" << std::endl;
+        std::cout << "Please choose an option:" << std::endl;
+        std::cout << "1. Change Number of slaves" << std::endl;
+        std::cout << "2. Print Oldest Log" << std::endl;
+        std::cout << "3. Print Nothing" << std::endl;
+        std::cout << "4. Make a new system" << std::endl;
+        std::cout << "5. Go Back" << std::endl;
+
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            std::cout << "How many slaves?" << std::endl;
+            changeSlaves();
+            break;
+        case 2:
+            std::cout << "You chose to Print Oldest Log" << std::endl;
+            // Code to make a change to the system
+            break;
+        case 3:
+            std::cout << "You chose to Print Nothing." << std::endl;
+            // Code to calibrate the system
+            break;
+        case 4: // Make a new system
+            std::cout << "You chose to make a new system." << std::endl;
+            // Code to make a new system
+            break;
+        case 5:
+            std::cout << "You chose to Go Back." << std::endl;
+            goBack = true;
+            break;
+        default:
+            std::cout << "Invalid choice. Please choose again." << std::endl;
+            break;
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+void PCHandler::clearScreen() const
+{
+    system("cls"); // Code to clear the screen
+}
+
 void PCHandler::changeSlaves() const
 {
     bool validChoice = false;
@@ -114,55 +170,4 @@ void PCHandler::changeSlaves() const
             std::cout << "Invalid choice. Please choose again." << std::endl;
         }
     }
-}
-
-void PCHandler::changeSystem() const
-{
-    std::cout << "Printing data..." << std::endl;
-
-    int choice = 0;
-    bool goBack = false;
-
-    while (!goBack)
-    {
-        clearScreen();
-        std::cout << "Welcome to your program!" << std::endl;
-        std::cout << "Please choose an option:" << std::endl;
-        std::cout << "1. Change Number of slaves" << std::endl;
-        std::cout << "2. Print Oldest Log" << std::endl;
-        std::cout << "3. Print Nothing" << std::endl;
-        std::cout << "4. Go Back" << std::endl;
-
-        std::cin >> choice;
-
-        switch (choice)
-        {
-        case 1:
-            std::cout << "How many slaves?" << std::endl;
-            changeSlaves();
-            break;
-        case 2:
-            std::cout << "You chose to Print Oldest Log" << std::endl;
-            // Code to make a change to the system
-            break;
-        case 3:
-            std::cout << "You chose to Print Nothing." << std::endl;
-            // Code to calibrate the system
-            break;
-        case 4:
-            std::cout << "You chose to Go Back." << std::endl;
-            goBack = true;
-            break;
-        default:
-            std::cout << "Invalid choice. Please choose again." << std::endl;
-            break;
-        }
-
-        std::cout << std::endl;
-    }
-}
-
-void PCHandler::clearScreen() const
-{
-    system("cls"); // Code to clear the screen
 }
