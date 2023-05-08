8436e8929bd80871f3c195c73b642174a7550754
Tukkel <terkel1234@gmail.com>
2023-05-08 11:38:07 +0200

    Log begyndt

---
+++ IPRS/app/master/lib/Log/Log.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+class Log
+{
+    public:
+        Log(uint8_t numberRooms, uint8_t numberPeople, double** roomHistory);
+    private:
+        int** rooms_;
+        double** roomChances_;
+        double** roomHistory_;
+        char** roomNames_;
+        char** peopleNames_;
+};
