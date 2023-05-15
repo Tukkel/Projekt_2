5335f27781879949424610ea60f45d6046c2cc61
Tukkel <terkel1234@gmail.com>
2023-05-15 14:13:11 +0200

    X10 videre

--- IPRS/app/master/lib/Log/Log.h
+++ IPRS/app/master/lib/Log/Log.h
@@ -9,13 +9,17 @@ class Log
 {
     public:
         Log(uint8_t numberRooms, uint8_t numberPeople, double** roomHistory);
-        void setRoomName(int roomNumber, char* roomName);
-        void setPersonName(int personNumber, char* personName);
-        void setAddress(size_t address, uint32_t addressType);
+        void setRoomConnection(uint8_t roomNumber, bool* connections);
+        void setRoomName(uint8_t roomNumber, char* roomName);
+        void setPersonName(size_t personNumber, char* personName);
+        void setAddress(uint8_t address, uint32_t addressType);
+        void logActivity(uint8_t roomNumber);
+        void logMovement(uint8_t roomNumber1, uint8_t roomNumber2);
     private:
-        int** rooms_;
+        bool** rooms_;
         double** roomChances_;
         double** roomHistory_;
+        bool** roomConnections_;
         char** roomNames_;
         char** peopleNames_;
         uint32_t* addresses_[256] = {300};
