3558615ca4431431be5bee0f949b4b23d3455e35
Junale <mr.simon.moller@gmail.com>
2023-05-17 12:10:46 +0200

    Set calibrate

--- IPRS/app/Sensor Slave/src/main.cpp
+++ IPRS/app/Sensor Slave/src/main.cpp
@@ -5,7 +5,7 @@ int main(void){
     uint8_t address[8] = {false};
     address[6] = true;
     SlaveSensor s(1, address, doorSensor);
-
+    s.calibrate();
     while (1)
     {
         s.updateActivity();
