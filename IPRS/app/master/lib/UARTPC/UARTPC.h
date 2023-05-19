f2a2eca7215652da7b64020d37de6b7284d97a85
Tukkel <terkel1234@gmail.com>
2023-05-19 11:41:12 +0200

    Master board UART try

--- IPRS/app/master/lib/UARTPC/UARTPC.h
+++ IPRS/app/master/lib/UARTPC/UARTPC.h
@@ -18,7 +18,7 @@ class UARTPC
 
 /****************
  * "~~ " Begynder
- * "~/8bit\!" Rummed folk
+ * "~/8bit\!" Rum med folk
  * Efter rum efterspørgelse "~~ Rumnavn som chars mellemrum antal som char mellemrum første navn som char !"
  * "~~ !" clear
  * 
