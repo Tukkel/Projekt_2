16705a56cab12919a504d6181e87f5d30f2d1927
Tukkel <terkel1234@gmail.com>
2023-05-19 14:21:39 +0200

    2 Main

--- IPRS/app/master/src/main.cpp
+++ IPRS/app/master/src/main.cpp
@@ -220,3 +220,61 @@ int main()
 	}
 	
 }
+
+/*
+#include <avr/io.h>
+#include "X10.h"
+#include "UARTPC.h"
+
+int main()
+{
+	UARTPC uartPc(9600, 8);
+	uint8_t hvad[8] = {0, 0, 0, 0, 0, 0, 0, 1};
+	uint8_t addresstwo[8] = {0, 0, 0, 0, 0, 0, 1, 0};
+	X10 x10(1, 2, 4, hvad, sizeof(hvad)/sizeof(hvad[0]), 'm');
+	uint8_t data[1] = {1};
+	uint8_t off[1] = {0};
+	uint8_t dataRead = 0;
+	DDRB = 0xFF;
+	bool recived = false;
+	while(true)
+	{
+		while(dataRead == 0)
+		{
+			while(recived == false)
+			{
+				recived = x10.writeData(data, sizeof(data)/sizeof(data[0]), addresstwo, sizeof(addresstwo)/sizeof(addresstwo[0]));
+				PORTB = 1;
+			}
+			recived = false;
+			x10.readData();
+			dataRead = x10.getValue();
+			PORTB = dataRead+1;
+		}
+		while(recived == false)
+		{
+			recived = x10.writeData(&dataRead, sizeof(&dataRead)/sizeof(&dataRead), hvad, sizeof(addresstwo)/sizeof(addresstwo[0]));
+			PORTB = 4;
+		}
+		recived = false;
+		dataRead = 0;
+		while(dataRead == 0)
+		{
+			while(recived == false)
+			{
+				recived = x10.writeData(data, sizeof(data)/sizeof(data[0]), addresstwo, sizeof(addresstwo)/sizeof(addresstwo[0]));
+				PORTB = 1;
+			}
+			recived = false;
+			x10.readData();
+			dataRead = x10.getValue();
+			PORTB = dataRead+1;
+		}
+		while(recived == false)
+		{
+			recived = x10.writeData(off, sizeof(off)/sizeof(off[0]), hvad, sizeof(addresstwo)/sizeof(addresstwo[0]));
+			PORTB = 4;
+		}
+	}
+}
+*/
