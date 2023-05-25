761f2c11d39ab97b6dd24db775498c3c58d6f83d
Simon_moller <sm@mercura.dk>
2023-05-25 18:05:24 +0200

    tilføjet main program til id sensor

--- IPRS/app/Sensor Slave/src/main.cpp
+++ IPRS/app/Sensor Slave/src/main.cpp
@@ -1,10 +1,12 @@
 #include "./Sensor Slave/slaveSensor.h"
 #include <avr/io.h>
 
-int main(void){
+
+// Main program for dør eller rum sensor
+/* int main(void){
     DDRB = 0xFF;
     uint8_t address[8] = {false};
-    address[6] = true;
+    address[7] = true;
     SlaveSensor s(1, address, doorSensor);
     s.calibrate();
 
@@ -14,6 +16,25 @@ int main(void){
         while(s.getActivity()){
             if (s.dataRequested()){
                 s.sendActivity();
+            }
+        }
+    }
+} */
+
+//Main program for id sensor
+int main(void){
+    DDRB = 0xFF;
+    uint8_t address[8] = {false};
+    address[7] = true;
+    SlaveSensor s(1, address, idSensor);
+    s.calibrate();
+
+    while (1)
+    {
+        s.updateIds();
+        while(s.getActivity()){
+            if (s.dataRequested()){
+                s.sendIds();
             }
         }
     }
